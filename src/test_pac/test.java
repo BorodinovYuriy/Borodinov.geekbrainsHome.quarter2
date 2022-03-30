f2b6de8f9bf497c15cccd9c78ef7f24307514f8c
Yuriy_Borodinov <aditone2378@gmail.com>
2022-03-30 21:41:37 +0300

    empty

--- src/test_pac/test.java
+++ src/test_pac/test.java
@@ -1,9 +1,6 @@
 package test_pac;
 
 public class test {
-    //это тест1
-    //это тест2
-    //это тест3
-    //таст совместимости1
+    //empty
 
 }
--- src/test_pac2/TestClass.java
+++
@@ -1,7 +0,0 @@
-package test_pac2;
-
-public class TestClass {
-    //test1
-    //test111
-
-}
